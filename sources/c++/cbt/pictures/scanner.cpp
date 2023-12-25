a38cf3c3a19b39072204c597ae49e9e3f2b2da19
Christophe Benne <christophe.benne@gmail.com>
2023-12-25 18:01:04 +0100

    #1: Build setup for CANON raw pictures import.

---
+++ sources/c++/cbt/pictures/scanner.cpp
@@ -0,0 +1,81 @@
+/**********************************************************************************
+ * MIT License                                                                    *
+ *                                                                                *
+ * Copyright © 2023 by Christophe Benne, this file is part of CBT software.       *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ * ********************************************************************************/
+
+/**********************************************************************************
+ * \file  scanner.cpp
+ * \brief Scanner class definitions file.
+ **********************************************************************************/
+
+#include "scanner.hpp"
+
+namespace cbt
+{
+
+    namespace pictures
+    {
+
+
+        /**
+         * \brief  Default constructor.
+         *
+         * Default constructor is private and not implemented.
+         */
+        Scanner::Scanner()
+        {
+        }
+
+        /**
+         * \brief  Copy constructor.
+         *
+         * Copy constructor is private and not implemented.
+         *
+         * \param  anObj An instance to be copied.
+         */
+        Scanner::Scanner(const Scanner& anObj)
+        {
+        }
+
+        /**
+         * \brief  Assignment operator.
+         *
+         * Assignment operator is private and not implemented.
+         *
+         * \param  anObj An instance to be copied.
+         */
+        Scanner& Scanner::operator=(const Scanner& anObj)
+        {
+	  return (*this);
+        }
+
+        /**
+         * \brief  Destructor.
+         */
+        Scanner::~Scanner()
+        {
+        }
+    }
+
+}
+
+
